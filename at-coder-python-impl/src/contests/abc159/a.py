dfaf7cb605c8b0658f438048c366b469f84a8a32
09x3086 <09x3086@gmail.com>
2020-03-28 14:35:17 +0900

    impl

---
+++ at-coder-python-impl/src/contests/abc159/a.py
@@ -0,0 +1,19 @@
+N, M = (int(x) for x in input().split())
+myList = []
+num = 0
+while num < N:
+    myList.append(2)
+    num += 1
+
+num = 0
+while num < M:
+    myList.append(1)
+    num += 1
+
+count = 0
+num = 0
+for i, j in enumerate(myList):
+    for k, v in enumerate(myList):
+        if k > i and (j + v) % 2 == 0:
+            count += 1
+print(count)
---
+++ at-coder-python-impl/src/contests/abc159/b.py
@@ -0,0 +1,8 @@
+N = input()
+
+L = []
+if len(N) % 2 == 1:
+    for S in N:
+        L.append(S)
+
+print(N)
