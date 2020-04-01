ae1ba2b80998bfcc95ebf4576d8b98e866d0a586
09x3086 <09x3086@gmail.com>
2020-04-02 05:47:59 +0900

    impl

--- at-coder-python-impl/src/contests/abc160/c.py
+++ at-coder-python-impl/src/contests/abc160/c.py
@@ -9,8 +9,9 @@ M = [int(i) for i in input().split()]
 
 a = 0
 for i in range(N[1] - 1):
-    a = a + M[i + 1] - M[i]
-
-
+    if abs(M[i + 1] - N[0] / 2) - M[i] >= N[0] / 2:
+        a = a + abs(M[i + 1] - N[0])
+    else:
+        a = a + M[i + 1] - M[i]
 
 print(a)
