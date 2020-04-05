78a673509791c849c819733637159921b478a35a
09x3086 <09x3086@gmail.com>
2020-04-05 13:32:59 +0900

    impl

--- at-coder-python-impl/src/contests/abc159/f.py
+++ at-coder-python-impl/src/contests/abc159/f.py
@@ -7,20 +7,21 @@ __author__ = "09x3086"
 N = [int(i) for i in input().split()]
 M = [int(i) for i in input().split()]
 
-dp = [[[0 for i in range(N[0] + 1)] for j in range(N[1] + 1)] for k in range(3)]
+dp = [[[0 for i in range(N[1] + 1)] for j in range(N[0] + 1)] for k in range(3)]
 dp[0][0][0] = 1
 
 p = 998244353
 
-for i in range(N[0] - 1):
-    for j in range(N[1] - 1):
+for i in range(N[0]):
+    for j in range(N[1] + 1):
         dp[0][i + 1][j] = (dp[0][i + 1][j] + dp[0][i][j]) % p
-        dp[1][i + 1][j] = (dp[1][i + 1][j] + dp[1][i][j]) % p
-        dp[2][i + 1][j] = (dp[2][i + 1][j] + dp[2][i][j]) % p
+        dp[1][i + 1][j] = (dp[1][i + 1][j] + dp[0][i][j] + dp[1][i][j]) % p
+        dp[2][i + 1][j] = (dp[2][i + 1][j] + dp[0][i][j] + dp[1][i][j] +
+                           dp[2][i][j]) % p
         if j + M[i] <= N[1]:
             dp[1][i + 1][j + M[i]] = (dp[1][i + 1][j + M[i]] + dp[0][i][j] +
                                       dp[1][i][j]) % p
-            dp[2][i + 1][j + M[i]] = (dp[2][i + 1][j + M[i]] + dp[i][j][0] +
+            dp[2][i + 1][j + M[i]] = (dp[2][i + 1][j + M[i]] + dp[0][i][j] +
                                       dp[1][i][j]) % p
 
-print(dp)
+print(dp[2][N[0]][N[1]])
